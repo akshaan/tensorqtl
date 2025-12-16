e0da43e2bfab5abc695ca5aeb50b13f3a8a2a3e9
Akshaan Kakar <akshaan.crackers@gmail.com>
2025-12-17 03:37:23 +0400

    Add push to build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,1 +1,2 @@
-docker build --platform  linux/amd64 -t akshaan22/tensorqtl-dev:latest .
+docker build --platform  linux/amd64 -t akshaan22/tensorqtl-dev:latest . && \
+docker push akshaan22/tensorqtl-dev:latest
