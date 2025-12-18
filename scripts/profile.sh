5e339b0c85d6311687b59613ec76e6f417557cd1
Akshaan Kakar <akshaan.crackers@gmail.com>
2025-12-19 00:59:57 +0400

    Add python sampling to prof command

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -5,6 +5,7 @@ nsys profile \
     --trace=cuda,nvtx,osrt,cudnn,cublas \
     --cudabacktrace=all \
     --python-backtrace=cuda \
+    --python-sampling=true \
     python3 -m tensorqtl example/data/GEUVADIS.445_samples.GRCh38.20170504.maf01.filtered.nodup.chr18 \
     example/data/GEUVADIS.445_samples.expression.bed.gz \
     GEUVADIS.445_samples \
