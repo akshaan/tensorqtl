c1dd46feb629fc24004f5d174418d6ebd5353733
Akshaan Kakar <akshaan.crackers@gmail.com>
2025-12-19 02:25:21 +0400

    Add args to profile script

--- scripts/profile.sh
+++ scripts/profile.sh
@@ -1,11 +1,34 @@
+COMPILE=''
+
+while getopts "c" opt; do
+  case $opt in
+    c) COMPILE=true ;;
+    \?) # Handle invalid options
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
+
+
+# Output dir
+
+OUTPUT_DIR=''
+if "$COMPILE"; then
+    OUTPUT_DIR='cis_compile'
+else
+    OUTPUT_DIR='cis_raw'
+fi
+mkdir -p OUTPUT_DIR
+
+   
+# Nsight profiling
+ 
 nsys profile \
     --stats=true \
     --force-overwrite true \
-    --output cis_torch_compile \
+    --output ${OUTPUT_DIR}/ \
     --trace=cuda,nvtx,osrt,cudnn,cublas \
-    --cudabacktrace=all \
-    --python-backtrace=cuda \
-    --python-sampling=true \
     python3 -m tensorqtl example/data/GEUVADIS.445_samples.GRCh38.20170504.maf01.filtered.nodup.chr18 \
     example/data/GEUVADIS.445_samples.expression.bed.gz \
     GEUVADIS.445_samples \
